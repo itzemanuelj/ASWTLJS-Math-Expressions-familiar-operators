99b8a9dad2be13c27c31ac431142c94dcf834273
Emanuel Joness <itzemanuelj@gmail.com>
2022-10-10 15:48:38 -0400

    mvp commit for math expressions.js

---
+++ script.js
@@ -0,0 +1,64 @@
+// Enter the operators, in this order, for addition, subtraction, multiplication, and division. Don't type spaces between them.
+// +-*/
+
+// What is the name of the arithmetic operator that gives you the remainder when one number is divided by another? (one word)
+modulus
+
+// Type the modulus operator.
+// %
+
+// What is the value of num?
+var num = 20 % 6;
+
+console.log(num);
+2
+
+// Form a statement that declares the variable costPerItem and assigns it the result of totalCost divided by numberOfItems.
+var costPerItem = totalCoast / numberOfItems;
+
+// In a single statement, declare the variable largeNum and assign it the result of 1,000 multiplied by 2,000.
+var largeNum = 1000 * 2000;
+
+// Assign to the variable num the result of dividing 9 by the value represented by the variable qty. The variable num hasn't been declared beforehand.
+var num = qty / 9;
+
+// Assign to a variable the result of adding a number to itself, for example 2 + 2. The variable hasn't been declared beforehand. Make up the variable name.
+var num = 2 + 2;
+
+// Assign to a variable the value represented by one variable subtracted from the value represented by another variable. The first variable has been declared beforehand. Make up all the variable names.
+var total = cost - time;
+
+// Code an alert that displays the result of any mathematical operation on 2 variables. Make up the variable names.
+alert(time / travel);
+
+// Assign to a variable the remainder when one number is divided by another. The variable hasn't been declared beforehand. Make up the variable name.
+var num = 20 % 2;
+
+// Assign to a variable the remainder when the value represented by a variable is divided by a number. The first variable has been declared beforehand. Make up the variable names.
+var cost = num / 2;
+
+// Time goal: 20 seconds. (The timer starts when you click in the answer field.) Enter the operators, in this order, for addition, subtraction, multiplication, division, and modulus. Don't type spaces between them. (The timer starts when you click in the answer field.)
+// +-*/%
+
+// Time goal: 25 seconds. Assign to a variable the result of adding a number to itself. The variable hasn't been declared beforehand. Make up the variable name.
+var num = 2 + 2;
+
+// Time goal: 30 seconds. Assign to a variable the value represented by one variable subtracted from the value represented by another variable. The first variable has been declared beforehand. Make up all the variable names.
+var total = cost - time;
+
+// Time goal: 30 seconds. Code an alert that displays the result of any mathematical operation on 2 variables. Make up the variable names.
+alert(time * cost);
+                            
+// Time goal: 30 seconds. Assign to a variable the remainder when one number is divided by another. The variable hasn't been declared beforehand. Make up the variable name.
+var total = 2 % 10;
+
+// Time goal: 30 seconds. Assign to a variable the remainder when the value represented by a variable is divided by a number. The first variable has been declared beforehand. Make up the variable names.
+var time = cost % 2;
+
+// Code an alert that displays the result of a math operation on 2 numbers.
+alert(1 + 2);
+
+// Assign numbers to 2 new variables. Code an alert that displays the result of a math operation on the variables.
+num1 = 1;
+num2 = 10;
+alert(num1 + num2);
